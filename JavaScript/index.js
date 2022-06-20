d69d70e9c2c7e11cc76c295c58640f01b18a84db
matbarn <mbmatbarn@gmail.com>
2022-06-20 13:26:36 +0100

    Add JS to voodoo to allow larger and shrink button

---
+++ JavaScript/index.js
@@ -0,0 +1,9 @@
+function big() {
+    var element = document.getElementById("img1");
+    element.style.height = "auto"
+}
+function small() {
+    var element = document.getElementById("img1");
+    element.style.height = "500px"
+}
+
